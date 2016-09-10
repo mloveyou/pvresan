9c7411e00516b95ba79b8b925725c7c5234a81d9
meysam <meysam547@gmail.com>
2016-09-10 21:39:39 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ import logging
 import subprocess
 import requests
 import os
-token = "270315659:AAGwXek7DzRZRrzSFPwgHxKrCzr7TpOATDM" #token
+token = "227816322:AAECksMDIO5_FTZsBfMRVx6pX47W0o4jBnQ" #token
 sudo = 109360529 #admin
 bot = telebot.TeleBot(token)
 db = "https://api.telegram.org/bot{}/getMe?".format(token)
